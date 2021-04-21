bad42bf3bd3cdd6483b0931374733ccf1aa144ca
kiki <1412730765@qq.com>
2021-04-21 21:23:04 +0800

    Site updated: 2021-04-21 21:23:01

--- js/jsdelivr.js
+++ js/jsdelivr.js
@@ -1,10 +1,1 @@
-var name = "aron-xx";
-var repository = "aron-xx.github.io";
-if(document.domain != 'localhost'){
-    for (var key in document.getElementById("articleContent").getElementsByTagName('img')){
-        if (isNaN(key)){
-            break;
-        }
-    	document.getElementById("articleContent").getElementsByTagName('img')[key].src = document.getElementById("articleContent").getElementsByTagName('img')[key].src.replace(document.domain,"cdn.jsdelivr.net/gh/" + name + "/" + repository)
-    }
-}
+"use strict";var name="aron-xx",repository="aron-xx.github.io";if("localhost"!=document.domain)for(var key in document.getElementById("articleContent").getElementsByTagName("img")){if(isNaN(key))break;document.getElementById("articleContent").getElementsByTagName("img")[key].src=document.getElementById("articleContent").getElementsByTagName("img")[key].src.replace(document.domain,"cdn.jsdelivr.net/gh/"+name+"/"+repository)}
